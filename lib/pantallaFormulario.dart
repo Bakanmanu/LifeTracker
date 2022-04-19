2d71e2ea5b10c4e36866ccf2f622a5f1e1d404d9
sergiomedinam98 <sergiomedinam@correo.ugr.es>
2022-04-19 20:13:52 +0200

    pantallaformulario

--- lib/pantallaFormulario.dart
+++ lib/pantallaFormulario.dart
@@ -3,6 +3,8 @@
 ///formulario.dart
 
 import 'package:flutter/material.dart';
+import 'package:practica2_ds/accion.dart';
+import "categoria.dart";
 
 class PantallaFormulario extends StatefulWidget{
 
@@ -12,14 +14,61 @@ class PantallaFormulario extends StatefulWidget{
 
 class _PantallaFormularioState extends State<PantallaFormulario>{
   @override
+  final List<Categoria> _categorias = [
+    Categoria(enunciado: '¿Cuánto has dormido?',
+        acciones: [Accion("1-3 horas", false), Accion("4-6 horas", false), Accion("6-8 horas", false), Accion("más de 8 horas", false)]),
+    Categoria(enunciado: '¿Qué has comido?',
+        acciones: [Accion("fruta", false), Accion("verdura", false), Accion("carne", false), Accion("pollo", false), Accion("pasta", false)]),
+  ];
+
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(title: Text("Pantalla Formulario"), backgroundColor: Colors.purple,),
       backgroundColor: Colors.purple,
         //todo modificar y rellenar
-      body: const Center(
-        child: Text("PANTALLA DE FORMULARIO"),
-      )
+      body: SafeArea(
+        child:
+          ListView(
+            children: [
+              Container(
+                margin: EdgeInsets.all(15),
+                padding: EdgeInsets.only(left: 15, right: 15, top: 15, bottom: 15),
+                decoration: BoxDecoration(color: Colors.purpleAccent,borderRadius: BorderRadius.circular(15),),
+                child:
+                Column(
+                  children: List.generate(
+                    _categorias.length,
+                        (indexCategorias){
+                      final categoria = _categorias[indexCategorias];
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(categoria.enunciado, style: TextStyle(color: Colors.white, fontSize: 20),),
+                        SizedBox(height: 15,),
+                        Wrap(
+                          children: List.generate(
+                              categoria.acciones.length,
+                                  (indexAcciones){
+                                return Row(
+                                  children: [
+                                    Checkbox(value: categoria.acciones[indexAcciones].activo, onChanged: (value){
+                                    }
+                                    ),
+                                    Text(categoria.acciones[indexAcciones].nombre, style: TextStyle(color: Colors.black, fontSize: 16)),
+                                  ],
+                                );
+
+                          }),
+                        )
+                      ],
+                    );
+                  },
+                  ),
+                ),
+              )
+            ],
+          ),
+      ),
     );
   }
 }
