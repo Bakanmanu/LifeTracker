d770c381d5422b226a560910ecf473e4f60390b6
Javidroid <javierfloridocartolano@gmail.com>
2022-04-19 14:34:39 +0200

    Creados los ficheros para los menús de los formularios -Javi

---
+++ lib/pantallaFormulario.dart
@@ -0,0 +1,3 @@
+///Este fichero sirve para crear la parte gráfica a la hora de crear
+///un nuevo formulario. Por ello, se relacionará directamente con el fichero
+///formulario.dart
---
+++ lib/pantallaMostrarFormularios.dart
@@ -0,0 +1,2 @@
+///Este fichero sirve para crear la parte gráfica a la hora de visualizar
+///los formularios.
