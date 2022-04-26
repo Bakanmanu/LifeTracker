3105b4a4478c0fb545044ef4941d58f55d109c26
sergiomedinam98 <sergiomedinam@correo.ugr.es>
2022-04-26 19:43:00 +0200

    pantallaFormulario_test

---
+++ test/pantallaFormulario_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:practica2_ds/main.dart';
+import 'package:practica2_ds/pantallaFormulario.dart';
+
+/// En este fichero se incluirán las pruebas de Widgets
+/// En este fichero se incluirán las pruebas de pantallaMenu
+///
+void main() {
+  testWidgets('Pruebas de Widgets de LifeTracker', (WidgetTester tester) async {
+    testWidgets('Prueba del botón Formulario nuevo', (WidgetTester tester) async {
+      // todo pruebas de widgets
+    });
+    testWidgets('Prueba del botón Buscar formulario', (WidgetTester tester) async {
+      // todo pruebas de widgets
+      //
+    });
+    testWidgets('Prueba del botón Editar Perfil', (WidgetTester tester) async {
+      // todo pruebas de widgets
+      //
+    });
