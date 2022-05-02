2301eea44a11e5c802e24ff2c376a6ef3a5516e7
Javidroid <javierfloridocartolano@gmail.com>
2022-05-02 22:44:38 +0200

    Cambios menores -Javi

--- lib/elementos_menu.dart
+++ lib/elementos_menu.dart
@@ -6,8 +6,8 @@ class Elemento{
   Elemento(this.id, this.nombre, this.imagen);
 }
 final menu = [
-  Elemento(1, "Formulario nuevo", "formulario.png"),
+  Elemento(1, "Formulario nuevo", "plus.png"),
   Elemento(2, "Buscar formulario", "buscador.png"),
   Elemento(3, "Editar perfil", "perfil.png"),
-  Elemento(4, "Gestionar categorias", "plus.png"),
+  Elemento(4, "Gestionar categorias", "categorias.png"),
 ];
