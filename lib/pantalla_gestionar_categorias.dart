dd84f4abdc8be9e2b041feabfe8edd4c193fd62f
Javidroid <javierfloridocartolano@gmail.com>
2022-05-02 21:50:32 +0200

    Añadido el menú para gestionar categorías -Javi

--- lib/pantalla_gestionar_categorias.dart
+++ lib/pantalla_gestionar_categorias.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 ///Este fichero sirve para crear la parte gráfica a la hora de
-///editar el perfil de usuario
+///editar las categorías
 
 class PantallaGestionarCategoria extends StatefulWidget{
   const PantallaGestionarCategoria({Key? key}) : super(key: key);
@@ -16,6 +16,14 @@ class _PantallaGestionarCategoriaState extends State<PantallaGestionarCategoria>
     return Scaffold(
         appBar: AppBar(title: const Text("Categorías"), backgroundColor: Colors.purple,),
         backgroundColor: Colors.purple,
+
+        /// LAS COSAS QUE HAY QUE GESTIONAR EN ESTA PANTALLA SON, RESPECTO A LAS CATEGORÍAS:
+        /// CREAR
+        /// EDITAR
+        /// BORRAR
+        /// IMPORTAR DESDE APPWEB
+        /// EXPORTAR (SUBIR) A APPWEB
+
         body: const Center(
           child: Text("ESTA CARACTERÍSTICA AÚN NO ESTÁ IMPLEMENTADA :(\n"
               "en un futuro se podrán crear, borrar, editar, importar y exportar packs de categorías"),
